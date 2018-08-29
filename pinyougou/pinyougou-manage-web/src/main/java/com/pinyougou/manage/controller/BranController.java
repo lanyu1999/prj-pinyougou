a4dac13255da2a5f25a5d5e029fbee07e8757b59
zyz <lanyu_1999@foxmail.com>
2018-08-29 19:14:05 +0800

    通用MAPPER 分页

--- pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BranController.java
+++ pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BranController.java
@@ -1,10 +1,12 @@
 package com.pinyougou.manage.controller;
 
+
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,5 +20,18 @@ public class BranController {
     @GetMapping("/findAll")
     public List<TbBrand> findAll() {
         return brandService.queryAll();
+    }
+
+    /**
+     * 根据分页信息分页查询品牌数据
+     * @param page 页号
+     * @param rows 页大小
+     * @return 品牌列表
+     */
+    @GetMapping("/testPage")
+    public List<TbBrand> testPage(@RequestParam(value="page", defaultValue = "1")Integer page,
+                                  @RequestParam(value="rows", defaultValue = "5")Integer rows){
+        //return brandService.testPage(page, rows);
+        return (List<TbBrand>) brandService.findByPage(page, rows).getRows();
     }
 }
--- pinyougou/pinyougou-sellergoods/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
+++ pinyougou/pinyougou-sellergoods/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -1,18 +1,26 @@
 package com.pinyougou.sellergoods.service.impl;
 import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.PageHelper;
 import com.pinyougou.mapper.BrandMapper;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
+import com.pinyougou.service.impl.BaseServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @Service(interfaceClass = BrandService.class)
-public class BrandServiceImpl  implements BrandService{
+public class BrandServiceImpl extends BaseServiceImpl <TbBrand> implements BrandService{
     @Autowired
     private BrandMapper brandMapper;
 
     public List<TbBrand> queryAll() {
         return brandMapper.queryAll();
+    }
+
+    @Override
+    public List<TbBrand> testPage(Integer page, Integer rows) {
+        PageHelper.startPage(page,rows);
+        return  brandMapper.selectAll();
     }
 }
