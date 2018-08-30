751f583a80960fb051df3b3553f61c7913760374
zyz <lanyu_1999@foxmail.com>
2018-08-30 08:50:21 +0800

    brand分页处理结束

--- pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BranController.java
+++ pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BranController.java
@@ -4,6 +4,7 @@ package com.pinyougou.manage.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
+import com.pinyougou.vo.PageResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,14 +25,21 @@ public class BranController {
 
     /**
      * 根据分页信息分页查询品牌数据
+     *
      * @param page 页号
      * @param rows 页大小
      * @return 品牌列表
      */
     @GetMapping("/testPage")
-    public List<TbBrand> testPage(@RequestParam(value="page", defaultValue = "1")Integer page,
-                                  @RequestParam(value="rows", defaultValue = "5")Integer rows){
+    public List<TbBrand> testPage(@RequestParam(value = "page", defaultValue = "1") Integer page,
+                                  @RequestParam(value = "rows", defaultValue = "5") Integer rows) {
         //return brandService.testPage(page, rows);
         return (List<TbBrand>) brandService.findByPage(page, rows).getRows();
+    }
+
+    @GetMapping("/findPage")
+    public PageResult findPage(@RequestParam(value = "page", defaultValue = "1") Integer page,
+                               @RequestParam(value = "rows", defaultValue = "5") Integer rows) {
+        return brandService.findByPage(page, rows);
     }
 }
