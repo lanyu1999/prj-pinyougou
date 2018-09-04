d934a94b73210ea63b22b49b9d9ee93543b519ec
zyz <lanyu_1999@foxmail.com>
2018-09-04 15:06:47 +0800

    商家登陆  权限拦截 - 密码加密 success day6

--- pinyougou/pinyougou-shop-web/src/main/java/com/pinyougou/shop/controller/SellerController.java
+++ pinyougou/pinyougou-shop-web/src/main/java/com/pinyougou/shop/controller/SellerController.java
@@ -5,6 +5,7 @@ import com.pinyougou.pojo.TbSeller;
 import com.pinyougou.sellergoods.service.SellerService;
 import com.pinyougou.vo.PageResult;
 import com.pinyougou.vo.Result;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,6 +32,8 @@ public class SellerController {
     public Result add(@RequestBody TbSeller seller) {
         try {
             seller.setStatus("0");//未审核
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            seller.setPassword(passwordEncoder.encode(seller.getPassword()));
             sellerService.add(seller);
             return Result.ok("增加成功");
         } catch (Exception e) {
