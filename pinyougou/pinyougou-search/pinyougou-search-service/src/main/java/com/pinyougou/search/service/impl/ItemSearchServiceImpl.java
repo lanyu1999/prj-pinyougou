c580b470ec1f381d23e2a20b3c9d2cb49f84d1ee
zyz <lanyu_1999@foxmail.com>
2018-09-12 19:44:01 +0800

    搜索功能实现,高亮显示完成  success day10

--- pinyougou/pinyougou-search/pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou/pinyougou-search/pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -6,15 +6,22 @@ import com.pinyougou.search.service.ItemSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
 import org.springframework.data.solr.core.query.Criteria;
+import org.springframework.data.solr.core.query.HighlightOptions;
+import org.springframework.data.solr.core.query.SimpleHighlightQuery;
 import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.result.HighlightEntry;
+import org.springframework.data.solr.core.query.result.HighlightPage;
 import org.springframework.data.solr.core.query.result.ScoredPage;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
 @Service(interfaceClass = ItemSearchService.class)
 public class ItemSearchServiceImpl implements ItemSearchService {
     @Autowired
     private SolrTemplate solrTemplate;
+
     /**
      * 根据搜索关键字搜索商品列表
      *
@@ -23,15 +30,35 @@ public class ItemSearchServiceImpl implements ItemSearchService {
      */
     @Override
     public Map<String, Object> search(Map<String, Object> searchMap) {
-            Map<String,Object> resultMap = new HashMap<>();
-        SimpleQuery query = new SimpleQuery();
+        Map<String, Object> resultMap = new HashMap<>();
+//        SimpleQuery query = new SimpleQuery();
+        //创建高亮搜索对象
+        SimpleHighlightQuery query = new SimpleHighlightQuery();
         //查询条件
         Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
         query.addCriteria(criteria);
+        //设置高亮
+        HighlightOptions highlightOptions = new HighlightOptions();
+        highlightOptions.addField("item_title");//高亮域
+        highlightOptions.setSimplePrefix("<em style='color:red'>");//高亮其实标签
+        highlightOptions.setSimplePostfix("</em>");//高亮结束标签
+        query.setHighlightOptions(highlightOptions);
         //查询
-        ScoredPage<TbItem> scoredPage = solrTemplate.queryForPage(query, TbItem.class);
+        HighlightPage<TbItem> itemHighlightPage = solrTemplate.queryForHighlightPage(query, TbItem.class);
+
+        // 处理高亮标题
+        List<HighlightEntry<TbItem>> highlighted = itemHighlightPage.getHighlighted();
+        if (highlighted != null && highlighted.size() > 0) {
+            for (HighlightEntry<TbItem> entry : highlighted) {
+                List<HighlightEntry.Highlight> highlights = entry.getHighlights();
+                if (highlights != null && highlights.size() > 0 && highlights.get(0).getSnipplets() != null) {
+                    // 设置高亮标题
+                    entry.getEntity().setTitle(highlights.get(0).getSnipplets().get(0));
+                }
+            }
+        }
         //设置返回的商品列表
-        resultMap.put("rows",scoredPage.getContent());
+        resultMap.put("rows", itemHighlightPage.getContent());
         return resultMap;
     }
 }
