99722d2473f9510a8f786c1588c0aade1ac936a7
zyz <lanyu_1999@foxmail.com>
2018-09-13 16:08:36 +0800

    过滤条件显示 /撤销  day 11

--- pinyougou/pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou/pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -1,10 +1,37 @@
 app.controller("searchController", function ($scope, searchService) {
+    // 搜索对象
+    $scope.searchMap = {"keywords": "", "category": "", "brand": "", "spec": {}};
     // 根据关键字搜索商品
-      $scope.search = function ()
-    {
+    $scope.search = function () {
         searchService.search($scope.searchMap).success(function (response) {
             $scope.resultMap = response;
         });
+    };
+
+// 添加过滤条件
+    $scope.addSearchItem = function (key, value) {
+        if ("brand" == key || "category" == key) {
+            // 如果点击的是品牌或者分类的话
+            $scope.searchMap[key] = value;
+        } else {
+            // 规格
+            $scope.searchMap.spec[key] = value;
+        }
+        // 点击过滤条件后需要重新搜索
+        $scope.search();
+    };
+
+    //删除过滤条件
+    $scope.removeSearchItem = function (key) {
+        if ("brand" == key || "category" == key) {
+            // 如果点击的是品牌或者分类的话
+            $scope.searchMap[key] = "";
+        } else {
+            // 规格
+           delete $scope.searchMap.spec[key];
+        }
+        // 点击过滤条件后需要重新搜索
+        $scope.search();
     }
-    ;
+
 });
