c54ac935818a9333fc187551058091531d22ae66
zyz <lanyu_1999@foxmail.com>
2018-09-13 20:29:54 +0800

    前端分页查询  success

--- pinyougou/pinyougou-search/pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou/pinyougou-search/pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -38,6 +38,19 @@ public class ItemSearchServiceImpl implements ItemSearchService {
         //查询条件
         Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
         query.addCriteria(criteria);
+        //设置分页信息
+        Integer pageNo = 1;
+        Integer pageSize = 20;
+        if(searchMap.get("pageNo")!=null){
+            pageNo = Integer.parseInt(searchMap.get("pageNo").toString());
+        }
+        if(searchMap.get("pageSize")!=null){
+            pageSize = Integer.parseInt(searchMap.get("pageSize").toString());
+        }
+        query.setOffset((pageNo-1));
+        query.setRows(pageSize);
+
+
         //设置高亮
         HighlightOptions highlightOptions = new HighlightOptions();
         highlightOptions.addField("item_title");//高亮域
@@ -45,27 +58,27 @@ public class ItemSearchServiceImpl implements ItemSearchService {
         highlightOptions.setSimplePostfix("</em>");//高亮结束标签
         query.setHighlightOptions(highlightOptions);
         //根据商品分类过滤查询:
-        if(!StringUtils.isEmpty(searchMap.get("category"))){
+        if (!StringUtils.isEmpty(searchMap.get("category"))) {
             Criteria categoryCriteria = new Criteria("item_category").is(searchMap.get("category"));
             SimpleFilterQuery simpleFilterQuery = new SimpleFilterQuery(categoryCriteria);
             query.addFilterQuery(simpleFilterQuery);
         }
-        if(!StringUtils.isEmpty(searchMap.get("brand"))){
+        if (!StringUtils.isEmpty(searchMap.get("brand"))) {
             Criteria brandCriteria = new Criteria("item_brand").is(searchMap.get("brand"));
             SimpleFilterQuery simpleFilterQuery = new SimpleFilterQuery(brandCriteria);
             query.addFilterQuery(simpleFilterQuery);
         }
-        if(searchMap.get("spec") != null){
-            Map<String,Object> map = (Map<String, Object>) searchMap.get("spec");
+        if (searchMap.get("spec") != null) {
+            Map<String, Object> map = (Map<String, Object>) searchMap.get("spec");
             Set<Map.Entry<String, Object>> entrySet = map.entrySet();
             for (Map.Entry<String, Object> entry : entrySet) {
-                Criteria specCriteria = new Criteria("item_spec_"+entry.getKey()).is(entry.getValue());
+                Criteria specCriteria = new Criteria("item_spec_" + entry.getKey()).is(entry.getValue());
                 SimpleFilterQuery simpleFilterQuery = new SimpleFilterQuery(specCriteria);
                 query.addFilterQuery(simpleFilterQuery);
             }
 
         }
-        if(!StringUtils.isEmpty(searchMap.get("price"))){
+        if (!StringUtils.isEmpty(searchMap.get("price"))) {
             //获取起始价格
             String[] prices = searchMap.get("price").toString().split("-");
             //价格大于等与起始价格
@@ -73,7 +86,7 @@ public class ItemSearchServiceImpl implements ItemSearchService {
             SimpleFilterQuery startPriceFilterQuery = new SimpleFilterQuery(startCriteria);
             query.addFilterQuery(startPriceFilterQuery);
             //价格雄安与等于结束价格
-            if(!"*".equals(prices[1])){
+            if (!"*".equals(prices[1])) {
                 //价格小于等于起始价格
                 Criteria endCriteria = new Criteria("item_price").lessThanEqual(prices[1]);
                 SimpleFilterQuery endPriceFilterQuery = new SimpleFilterQuery(endCriteria);
@@ -97,6 +110,8 @@ public class ItemSearchServiceImpl implements ItemSearchService {
         }
         //设置返回的商品列表
         resultMap.put("rows", itemHighlightPage.getContent());
+        resultMap.put("totalPages",itemHighlightPage.getTotalPages());
+        resultMap.put("total",itemHighlightPage.getTotalElements());
         return resultMap;
     }
 }
